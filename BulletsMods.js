e8dd3d764c51a8da4b1c8389c66c636d3b4bfeb3
VNBPM on YT <77667370+iBLiSSIN@users.noreply.github.com>
2021-11-15 17:33:23 +0700

    Update and rename BulletsMods to BulletsMods.js

---
+++ BulletsMods.js
@@ -0,0 +1,25 @@
+// Recolor for bullets
+ 
+(function() {
+    'use strict';
+var ammo = ""
+var func = {
+    webpack_inject: (w, e, get) => {
+        ammo = get("764654e6")
+    },
+};
+if(typeof window.webpackJsonp === 'function') {
+    window.webpackJsonp([0], func, ["webpack_inject"]);
+} else {
+    window.webpackJsonp.push([
+        ["webpack_inject"],
+        func,
+        [["webpack_inject"]]
+    ]);
+}
+Object.keys(ammo).forEach(function(key) {
+    if(ammo[key].type === "ammo") {
+        ammo[key].worldImg.sprite =  "https://raw.githubusercontent.com/iBLiSSIN/SurvivMods/main/img%20for%20change/6A7B426F-8765-41B3-B134-9BBF9683029F.png"
+        }
+})
+})();
